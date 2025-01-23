d7de30697324cb7bed15ff7727a069a89fcd80de
rayanussen <rayanscp@gmail.com>
2025-01-24 00:42:47 +0100

    Commit Rayan

---
+++ script.py
@@ -0,0 +1,18 @@
+def main():
+    print(" Hi! My name is Rayan Ussen, and I am currently pursuing a degree in Economics at IE University in Madrid.")
+    print("I chose Economics because of the International Baccalaureate (IB) course I took during high school.")
+    print()
+    print("My Hobbies:")
+    print("1. Watching and playing sports: From football to basketball, I enjoy both being on the field and cheering from the sidelines or at the TV")
+    print("2. Having fun with my friends: Spending time with friends is something I value.")
+    print("3. Playing video games: Gaming helps me relax and unwind after a busy day.")
+    print()
+    print(" Why Am I Studying Economics?")
+    print("Economics fascinates me because it’s about understanding human behavior, decision-making, and how markets shape the world.")
+    print("The IB Economics course introduced me to these ideas, and now, at IE University, I am diving deeper into these concepts.")
+    print()
+    print("  Note:")
+    print("I'm also learning how to code in Python and exploring tools like PyCharm and GitHub. Combining tech with Economics is exciting but very hard still for me!")
+
+if __name__ == "__main__":
+    main()
